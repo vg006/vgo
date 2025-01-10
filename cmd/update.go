0ceee28ad3e5f14e1e517d8349e48a35f03583ec
Vganesh <savijayganesh@gmail.com>
2025-01-10 21:33:35 +0530

    ✨ feat: Added command to update

---
+++ cmd/update.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/charmbracelet/huh/spinner"
+	"github.com/spf13/cobra"
+	asset "github.com/vg006/vgo/internal/assets"
+)
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
+
+var updateCmd = &cobra.Command{
+	Use:   "up",
+	Short: "Update the vgo tool to the latest version",
+	Run: func(cmd *cobra.Command, args []string) {
+		_ = spinner.
+			New().
+			Title("Updating vgo ...").
+			Action(func() {
+				_, err := exec.Command("go", "install", "github.com/vg006/vgo@latest").Output()
+				if err != nil {
+					cmd.Println(
+						asset.Text.Foreground(asset.Red).
+							Render(fmt.Sprintf("%s Error : Failed to update the vgo tool", asset.EmojiError)))
+				}
+				cmd.Println(
+					asset.Text.Foreground(asset.Green).
+						Render(fmt.Sprintf("%s Yupee! You are now up-to-date!", asset.EmojiSparkles)))
+			}).
+			Style(asset.Text).
+			Accessible(false).
+			Run()
+	},
+}
