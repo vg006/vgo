70720018d058586ca5f493cbd0c4a083798f18c9
Vganesh <savijayganesh@gmail.com>
2025-01-10 21:31:52 +0530

    🎨 style: Updated build UI

--- cmd/build.go
+++ cmd/build.go
@@ -3,31 +3,47 @@ package cmd
 import (
 	"os/exec"
 
+	"github.com/charmbracelet/huh/spinner"
 	"github.com/spf13/cobra"
+	asset "github.com/vg006/vgo/internal/assets"
 )
 
 func init() {
-	rootCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(buildCmd)
 }
 
-var updateCmd = &cobra.Command{
-	Use:   "up",
-	Short: "Update the vgo tool to the latest version",
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "Build the vgo tool and install it",
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Println("󰑮 Building")
-		_, err := exec.Command("go", "build").Output()
-		if err != nil {
-			cmd.Println(" Error : Failed to update the vgo tool", err)
-			return
-		}
-		cmd.Println(" Built")
-		cmd.Println("󰑮 Installing")
-		_, err = exec.Command("go", "install").Output()
-		if err != nil {
-			cmd.Println(" Error : Failed to update the vgo tool", err)
-			return
-		}
-		cmd.Println(" Installed")
+		_ = spinner.
+			New().
+			Title("Building vgo ...").
+			Action(func() {
+				_, err := exec.Command("go", "build").Output()
+				if err != nil {
+					cmd.Printf("%s Error : Failed to update the vgo tool", asset.EmojiError)
+					return
+				}
+				cmd.Printf("%s Built", asset.EmojiTick)
+			}).
+			Style(asset.Text).
+			Accessible(true).
+			Run()
 
+		_ = spinner.
+			New().
+			Title("Installing vgo ...").
+			Action(func() {
+				_, err = exec.Command("go", "install").Output()
+				if err != nil {
+					cmd.Printf("%s Error : Failed to update the vgo tool", asset.EmojiError)
+					return
+				}
+				cmd.Printf("%s Installed", asset.EmojiTick)
+			}).
+			Style(asset.Text).
+			Accessible(true).
+			Run()
 	},
 }
